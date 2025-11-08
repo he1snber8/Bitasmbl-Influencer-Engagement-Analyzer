4e4363a24599f6142db488691516a8913f4147bf
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-11-08 19:28:49 +0400

    Add App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+// App shell
+import React from 'react'
+import InfluencerChart from './components/InfluencerChart'
+
+export default function App(){
+  return (
+    <div>
+      <h1>Influencer Engagement Analyzer</h1>
+      <InfluencerChart />
+    </div>
+  )
+}
